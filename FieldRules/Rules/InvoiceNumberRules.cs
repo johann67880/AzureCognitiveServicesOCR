ad1d3c8091758fe24d8c31b4832865511ae8d8a0
Johann Alberto Jimenez Caraballo <johaljicar@hotmail.com>
2020-04-20 19:17:22 -0500

    Update InvoiceNumberRules.cs

--- FieldRules/Rules/InvoiceNumberRules.cs
+++ FieldRules/Rules/InvoiceNumberRules.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace FieldRules
 {
@@ -11,8 +12,54 @@ namespace FieldRules
         {
             return new List<Func<bool>>
             {
-                //TODO: Define here all the rules needed to validate if value is invoice number
+                //You can add as many rules as you want
+                () => IsInvoiceNumber(value)
             }.Any(number => number());
+        }
+
+        public static bool IsInvoiceNumber(string value)
+        {
+            List<string> fieldName = new List<string>()
+            {
+                "Factura de venta",
+                "Pedido",
+                "Remision",
+                "Número"
+            };
+
+            //Only process value when contains one of the FieldName.
+            if (!fieldName.Contains(value))
+                return false;
+
+            string result = ExtractInvoiceNumber(value);
+
+            List<string> validFormats = new List<string>()
+            {
+                @"(.|\s)*\S(.|\s)*"
+            };
+
+            //returns true when at least one pattern is valid.
+            return validFormats.Any(x => Regex.IsMatch(result, x));
+        }
+
+        public static string ExtractInvoiceNumber(string value)
+        {
+            //When is NIT, then remove all the follow words or symbols.
+            List<string> rulesList = new List<string>()
+            {
+                "N°",
+                "NO",
+                ".",
+                " "
+            };
+
+            string result = string.Empty;
+
+            rulesList.ForEach(x =>
+                result = value.Replace(x, string.Empty).Trim()
+            );
+
+            return result;
         }
     }
 }
