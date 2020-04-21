97db0b77ff34392109735c979ed1e45abef310b0
Johann Alberto Jimenez Caraballo <johaljicar@hotmail.com>
2020-04-20 19:16:57 -0500

    Update InvoiceDateRules.cs

--- FieldRules/Rules/InvoiceDateRules.cs
+++ FieldRules/Rules/InvoiceDateRules.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace FieldRules
 {
@@ -13,6 +14,62 @@ namespace FieldRules
             {
                 //TODO: Define here all the rules needed to validate if value is invoice date
             }.Any(date => date());
+        }
+
+        public static bool IsDate(string value)
+        {
+            List<string> fieldName = new List<string>()
+            {
+                "Fecha",
+                "Expedicion"
+            };
+
+            //Only process value when contains one of the FieldName.
+            if (!fieldName.Contains(value))
+                return false;
+
+            //Regular Expressions to determine if is a valid NIT
+            List<string> validFormats = new List<string>()
+            {
+                //expression to validate dates in dd/mm/yyyy, dd-mm-yyyy or dd.mm.yyyy
+                @"^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$",
+                //expression to validate dates in yyyy-mm-dd
+                @"([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))",
+                //expression to validate dates in yyyy/mm/dd
+                @"([12]\d{3}/(0[1-9]|1[0-2])/(0[1-9]|[12]\d|3[01]))",
+                //expression to validate dates in yyyy.mm.dd
+                @"([12]\d{3}.(0[1-9]|1[0-2]).(0[1-9]|[12]\d|3[01]))"
+            };
+
+            string result = ExtractDate(value);
+
+            //returns true when at least one pattern is valid.
+            return validFormats.Any(x => Regex.IsMatch(result, x));
+        }
+
+        public static string ExtractDate(string value)
+        {
+            //When is NIT, then remove all the follow words or symbols.
+            List<string> rulesList = new List<string>()
+            {
+                "Fecha",
+                "Expedicion",
+                "Ciudad",
+                "factura",
+                "exped",
+                "comprobante",
+                "de",
+                ":",
+                " "
+            };
+
+            string result = string.Empty;
+
+            rulesList.ForEach(x =>
+                result = value.Replace(x, string.Empty).Trim()
+            );
+
+            return result;
         }
     }
 }
