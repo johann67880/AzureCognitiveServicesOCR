997b4bd4dac0e755c6e1a61c8510f79fc7e43298
Johann Alberto Jimenez Caraballo <johaljicar@hotmail.com>
2020-04-20 19:18:09 -0500

    Update SubtotalRules.cs

--- FieldRules/Rules/SubtotalRules.cs
+++ FieldRules/Rules/SubtotalRules.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace FieldRules
 {
@@ -11,8 +12,53 @@ namespace FieldRules
         {
             return new List<Func<bool>>
             {
-                //TODO: Define here all the rules needed to validate if value is subtotal
+                //You can add as many rules as you want
+                () => IsSubtotal(value)
             }.Any(subtotal => subtotal());
+        }
+
+        public static bool IsSubtotal(string value)
+        {
+            List<string> fieldName = new List<string>()
+            {
+                "SUBTOTAL"
+            };
+
+            //Only process value when contains one of the FieldName.
+            if (!fieldName.Contains(value))
+                return false;
+
+            //Regular Expressions to determine if is a valid NIT
+            List<string> validFormats = new List<string>()
+            {
+                "^[0-9.]*$"
+            };
+
+            string result = ExtractSubtotal(value);
+
+            //returns true when at least one pattern is valid.
+            return validFormats.Any(x => Regex.IsMatch(result, x));
+        }
+
+        public static string ExtractSubtotal(string value)
+        {
+            //When is NIT, then remove all the follow words or symbols.
+            List<string> rulesList = new List<string>()
+            {
+                "SUBTOTAL",
+                "$",
+                ".",
+                ",",
+                " "
+            };
+
+            string result = string.Empty;
+
+            rulesList.ForEach(x =>
+                result = value.Replace(x, string.Empty).Trim()
+            );
+
+            return result;
         }
     }
 }
